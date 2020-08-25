66cda328afc300256870c724e3747de996358e88
Antônio Bretas <toninho_vasco94@hotmail.com>
2020-08-25 02:23:45 -0300

    Exercício Python HackerRank

--- Exercício/findtherunnerupscore.py
+++ Exercício/findtherunnerupscore.py
@@ -1,8 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    maxi = max(arr)
-for i in range(len(arr)):
-    if maxi in arr:
-        arr.remove(maxi)
-print(max(arr))
+    result = list(set(arr)) #set remove valores duplicados
+    result.remove(max(result)) #remove o maior valor
+    print(max(result))
--- Exercício/re.start-re.end.py
+++ Exercício/re.start-re.end.py
@@ -1,9 +1,10 @@
 import re
 s = input()
-k = re.compile(input())
+k = re.compile(input())#compile junta RE ao objeto facilitando busca
 m = k.search(s)
 if not m:
-    print ("(-1, -1)")
+    print ((-1, -1))
 while m:
     print ((m.start(), m.end()-1))
+    #print ("({0}, {1})".format(m.start(), m.end()-1))
     m = k.search(s,m.start()+1)
